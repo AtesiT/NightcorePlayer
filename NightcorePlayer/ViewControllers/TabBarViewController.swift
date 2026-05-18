cf4100408c7af748622f2095616e467b81a47922
AtesiT <atesit@mail.ru>
2026-05-18 23:57:33 +0300

    ADD: CollectionVC in TabBarVC

--- NightcorePlayer/ViewControllers/TabBarViewController.swift
+++ NightcorePlayer/ViewControllers/TabBarViewController.swift
@@ -11,13 +11,21 @@ final class TabBarViewController: UITabBarController {
     }
     
     private func setToolBar() {
-        let theFirstViewController = UIViewController()
-        theFirstViewController.view.backgroundColor = .systemBackground
-        theFirstViewController.title = "Music"
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
         
-        let theFirstNavigationController = UINavigationController(rootViewController: theFirstViewController)
-        theFirstNavigationController.tabBarItem = UITabBarItem(title: "Music", image: UIImage(systemName: "music.note"), tag: 0)
+        let theFirstCollectionVC = CollectionViewController(collectionViewLayout: layout)
+        theFirstCollectionVC.title = "Music List"
         
-        viewControllers = [theFirstNavigationController]
+        let theFirstVC = UIViewController()
+        theFirstVC.view.backgroundColor = .systemBackground
+        theFirstVC.title = "Music"
+        
+        let theNavigationCollectionController = UINavigationController(rootViewController: theFirstCollectionVC)
+        let theNavigationViewController = UINavigationController(rootViewController: theFirstVC)
+        theNavigationCollectionController.tabBarItem = UITabBarItem(title: "Music list", image: UIImage(systemName: "music.note.list"), tag: 0)
+        theNavigationViewController.tabBarItem = UITabBarItem(title: "Music", image: UIImage(systemName: "music.note"), tag: 1)
+        
+        viewControllers = [theNavigationCollectionController, theNavigationViewController]
     }
 }
