e4cf1b3f8c3e80ef1c926e7e1ef1309027af16c2
AtesiT <atesit@mail.ru>
2026-05-22 17:32:34 +0300

    ADD: struct Music and mock data in musicArray

---
+++ NightcorePlayer/Model/Music.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+struct Music {
+    let artist: String
+    let title: String
+    let image: String
+    let duration: String
+}
+
+//  MARK: - Mock Data
+
+let musicArray = [
+    Music(artist: "Music", title: "First", image: "music.note", duration: "1:00"),
+    Music(artist: "Music", title: "Second", image: "music.note", duration: "2:00"),
+    Music(artist: "Music", title: "Third", image: "music.note", duration: "3:00"),
+    Music(artist: "Music", title: "Fourth", image: "music.note", duration: "4:00"),
+    Music(artist: "Music", title: "Fiveth", image: "music.note", duration: "5:00"),
+]
