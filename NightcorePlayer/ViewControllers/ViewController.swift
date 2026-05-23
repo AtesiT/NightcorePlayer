c333f7aff8d418ca549e22d3010227f50200c5c7
AtesiT <atesit@mail.ru>
2026-05-23 15:50:19 +0300

    ADD: name track and artist track labels on VC

--- NightcorePlayer/ViewControllers/ViewController.swift
+++ NightcorePlayer/ViewControllers/ViewController.swift
@@ -16,10 +16,25 @@ final class ViewController: UIViewController {
         return imageView
     }()
     
+    private lazy var nameTrack: UILabel = {
+        let label = UILabel()
+        label.text = "Name of a track"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var artistTrack: UILabel = {
+        let label = UILabel()
+        label.text = "Name of an artist"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(trackImageView)
+        view.addSubview(nameTrack)
+        view.addSubview(artistTrack)
         setConstraints()
     }
 }
@@ -30,8 +45,16 @@ extension ViewController {
             trackImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 60),
             trackImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -60),
             trackImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 60),
+            //  Сделать высоту равную ширине картинки (квадратную картину делаем)
+            trackImageView.heightAnchor.constraint(equalTo: trackImageView.widthAnchor),
             
-            trackImageView.heightAnchor.constraint(equalTo: trackImageView.widthAnchor)
+            nameTrack.topAnchor.constraint(equalTo: trackImageView.bottomAnchor, constant: 20),
+            nameTrack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -60),
+            nameTrack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 60),
+            
+            artistTrack.topAnchor.constraint(equalTo: nameTrack.bottomAnchor, constant: 20),
+            artistTrack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -60),
+            artistTrack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 60),
         ])
     }
 }
